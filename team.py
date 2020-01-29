c237b41246a635f3235a0b32d6f994829161af6b
infsmrt <Codathon2020>
2020-01-29 17:47:55 +0000

    some change on test

--- team.py
+++ team.py
@@ -5,19 +5,17 @@ import numpy as np
 
 def find(branch, position):
     # Load dataset
-    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-    dataset = read_csv(url, names=names)
+    dataset = read_csv("training.csv", header=0)
 
     # Split-out validation dataset
     array = dataset.values
-    X = array[:,0:4]
-    y = array[:,4]
+    X = array[:,0:2]
+    y = array[:,2]
 
     # Make predictions on validation dataset
     model = SVC(gamma='auto')
     model.fit(X, y)
 
-    data = [[6.8, 4.0, 1.2, 2]]
+    data = [[branch, position]]
     predictions = model.predict(data)
     return np.array2string(predictions)
