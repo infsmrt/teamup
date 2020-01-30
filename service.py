b1446790db5f0ec55dbde477dd1c9b9211d8996e
user-infsmrt <jasontan323@ymail.com>
2020-01-30 00:29:02 +0000

    Added CORS

--- service.py
+++ service.py
@@ -1,12 +1,16 @@
 import flask
 from flask import request
+from flask_cors import CORS
 
 import team
 
 app = flask.Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def find():
-    return team.find( int(request.args['branch']), int(request.args['position']) )
+    return {
+        'Names': team.find( int(request.args['branch']), int(request.args['position']) )
+    }
 
 app.run()
